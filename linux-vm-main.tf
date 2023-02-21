5d4d61055d9766c06861d0f7352bf2bce4d6a760
harish <86610464+nagaharish5@users.noreply.github.com>
2023-02-21 19:41:35 +0530

    removed few
    removed elasticip and gave my key name,removed extra disk

--- linux-vm-main.tf
+++ linux-vm-main.tf
@@ -2,14 +2,7 @@
 ## Virtual Machine Module - Main ##
 ###################################
 
-# Create Elastic IP for the EC2 instance
-resource "aws_eip" "linux-eip" {
-  vpc  = true
-  tags = {
-    Name        = "${lower(var.app_name)}-${var.app_environment}-linux-eip"
-    Environment = var.app_environment
-  }
-}
+
 
 # Create EC2 Instance
 resource "aws_instance" "linux-server" {
@@ -19,7 +12,7 @@ resource "aws_instance" "linux-server" {
   vpc_security_group_ids      = [aws_security_group.aws-linux-sg.id]
   associate_public_ip_address = var.linux_associate_public_ip_address
   source_dest_check           = false
-  key_name                    = aws_key_pair.key_pair.key_name
+  key_name                    = ec2-key-new-account
   user_data                   = file("aws-user-data.sh")
   
   # root disk
@@ -29,26 +22,11 @@ resource "aws_instance" "linux-server" {
     delete_on_termination = true
     encrypted             = true
   }
-
-  # extra disk
-  ebs_block_device {
-    device_name           = "/dev/xvda"
-    volume_size           = var.linux_data_volume_size
-    volume_type           = var.linux_data_volume_type
-    encrypted             = true
-    delete_on_termination = true
-  }
   
   tags = {
     Name        = "${lower(var.app_name)}-${var.app_environment}-linux-server"
     Environment = var.app_environment
   }
-}
-
-# Associate Elastic IP to Linux Server
-resource "aws_eip_association" "linux-eip-association" {
-  instance_id   = aws_instance.linux-server.id
-  allocation_id = aws_eip.linux-eip.id
 }
 
 # Define the security group for the Linux server
